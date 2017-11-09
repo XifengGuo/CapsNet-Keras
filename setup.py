b8cc5b8179ef245aecc6c0bd7e408d75455f3eab
John Henning <jlhbaseball15@me.com>
2017-11-08 20:12:43 -0500

    refactored into an installable python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='CapsNet-Keras',
+    version='',
+    packages=['keras_capsules'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    requires=[
+        'tensorflow',
+        'keras'
+    ]
+)
