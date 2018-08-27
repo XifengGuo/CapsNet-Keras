0621dd51ec5c06e9c54e2edfaa0f8b9691faf1f6
iretiayo <iretibob@gmail.com>
2018-08-27 07:20:38 -0400

    modified the plot_log function to have a consistent behaviour. data columns are now accessed by the column names; this does not assume the first column is epoch as before (#70)

--- utils.py
+++ utils.py
@@ -2,39 +2,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 import csv
 import math
+import pandas
 
 def plot_log(filename, show=True):
-    # load data
-    keys = []
-    values = []
-    with open(filename, 'r') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            if keys == []:
-                for key, value in row.items():
-                    keys.append(key)
-                    values.append(float(value))
-                continue
 
-            for _, value in row.items():
-                values.append(float(value))
-
-        values = np.reshape(values, newshape=(-1, len(keys)))
-        values[:,0] += 1
+    data = pandas.read_csv(filename)
 
     fig = plt.figure(figsize=(4,6))
     fig.subplots_adjust(top=0.95, bottom=0.05, right=0.95)
     fig.add_subplot(211)
-    for i, key in enumerate(keys):
+    for key in data.keys():
         if key.find('loss') >= 0 and not key.find('val') >= 0:  # training loss
-            plt.plot(values[:, 0], values[:, i], label=key)
+            plt.plot(data['epoch'].values, data[key].values, label=key)
     plt.legend()
     plt.title('Training loss')
 
     fig.add_subplot(212)
-    for i, key in enumerate(keys):
+    for key in data.keys():
         if key.find('acc') >= 0:  # acc
-            plt.plot(values[:, 0], values[:, i], label=key)
+            plt.plot(data['epoch'].values, data[key].values, label=key)
     plt.legend()
     plt.title('Training and validation accuracy')
 
